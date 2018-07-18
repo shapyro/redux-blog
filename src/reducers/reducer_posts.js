d1900a4febf7f475b5aed7c493c300f1d7b66eec
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2018-07-17 22:49:54 -0500

    lodash map posts

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -3,7 +3,7 @@ import { FETCH_POSTS } from '../actions'
 export default function(state = {}, action) {
   switch(action.type) {
   case FETCH_POSTS:
-    console.log(action.payload.data); // [ post1, post2 ]
+    return action.payload.data; // [ post1, post2 ]
   default:
     return state;
   }
