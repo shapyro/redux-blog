57142c5d507b60bbc6e40676b21ff47977f7e5d3
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2018-07-17 19:00:49 -0500

    adding to git

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
+
+import PostsIndex from './components/posts_index';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
