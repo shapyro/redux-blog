dc7ed7b58d6cffd60336412b1f670918c58d531e
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2018-07-26 19:23:41 -0500

    can add or remove posts

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,16 +1,14 @@
-import { FETCH_POSTS, FETCH_POST } from '../actions'
+import _ from 'lodash';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions'
 
 export default function(state = {}, action) {
   switch(action.type) {
+  case DELETE_POST:
+    return _.omit(state, action.payload)
   case FETCH_POST:
-    // const post = action.payload.data;
-    // const newState = { ...state };
-    // newState[post.id] = post;
-    // return newState;
-    
     return { ...state, [action.payload.data.id]: action.payload.data }
   case FETCH_POSTS:
-    return action.payload.data; // [ post1, post2 ]
+    return _.mapKeys(action.payload.data, 'id'); // [ post1, post2 ]
   default:
     return state;
   }
